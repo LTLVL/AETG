dd897ed541acebd7cb79ceed250c0bd452709d1c
Liu <1184690200@qq.com>
2023-12-06 13:22:13 +0800

    Initial commit

---
+++ src/main/java/org/zju/domain/Laptop.java
@@ -0,0 +1,20 @@
+package org.example.domain;
+
+import lombok.Data;
+
+@Data
+public class Laptop {
+    private String brand;
+    private String aspectRatio;
+    private String energyEfficiencyClass;
+    private String SSD;
+    private String thicknesses;
+    private String fuselageMaterial;
+    private String mechanicalDrive;
+    private String RAM;
+    private String screenSize;
+    private String screenGamut;
+    private String screenRefreshRate;
+    private String characterisation;
+    private String series;
+}
---
+++ src/main/java/org/zju/util/Algorithm.java
@@ -0,0 +1,2 @@
+package org.zju.util;public class Algorithm {
+}
